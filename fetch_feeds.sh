40b24649274764c46e0ce87d4a40e557dbbd4210
Marc Dequènes (Duck) <duck@redhat.com>
2021-10-18 23:20:17 +0900

    Pass plutot update options after the subcommand

--- fetch_feeds.sh
+++ fetch_feeds.sh
@@ -2,5 +2,5 @@
 set -e
 
 cd /srv/pluto/
-bundle exec pluto -c $GITHUB_WORKSPACE/planet.ini -d $GITHUB_WORKSPACE/planet.db update
+bundle exec pluto -c $GITHUB_WORKSPACE/planet.ini update -d $GITHUB_WORKSPACE/planet.db
 
